5aa3f68261772c6554b8a55bc4f36d513fc0e2fa
SAFA <sammoudasaffa@gmail.com>
2020-08-19 11:14:00 -0400

    chargement style

--- functions.php
+++ functions.php
@@ -3,4 +3,4 @@ add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'style', get_template_directory_uri().'/style.css' );
 }
-?><
+?>
--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document avec git</title>
+    <?php wp_head()?>
 </head>
 <body>
     <h1>Hello</h1>
+    <?php wp_footer()?>
 </body>
 </html>
